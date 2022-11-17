b013eedb6ff42300e4daf571a4128cba1627fb31
Seth Faerber <seth.faerber@ramseysolutions.com>
2022-11-17 15:52:34 -0600

    Lesson 19: Handling Validation Errors in UI

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,14 +18,20 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
