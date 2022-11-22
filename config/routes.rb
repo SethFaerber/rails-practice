dd7276c4a63d9b79feb89672325813f8ea884878
Seth Faerber <seth.faerber@ramseysolutions.com>
2022-11-22 10:13:01 -0600

    Lesson 23: Reviews rendered

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
 
   root "movies#index"
 
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
 
   resources :nineties
 
