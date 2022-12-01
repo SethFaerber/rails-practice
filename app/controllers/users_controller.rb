13825ea4c46f06c92483ff9c88caab14c4e4c051
Seth Faerber <seth.faerber@ramseysolutions.com>
2022-12-01 13:31:35 -0600

    Lesson 33: Authorization 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
 
   def index
     @users = User.all
@@ -25,11 +27,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account Updated!"
     else
@@ -38,7 +38,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to movies_url,
@@ -51,5 +50,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).
       permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url, status: :see_other unless current_user?(@user)
   end
 end
